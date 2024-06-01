a84b4ac54b6806008d6e371a2ffcd6eecfdb794c
SolomonOnwueyikpe <solofamily2003@yahoo.com>
2024-06-01 01:54:22 +0000

    ES6 Classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,13 +1,22 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this._range = range;
+    this.range = range;
   }
 
-  // eslint-disable-next-line class-methods-use-this
+  get range() {
+    return this._range;
+  }
+
+  set range(val) {
+    this._range = val;
+  }
+
   cloneCar() {
-    return new Car();
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
   }
 }
